d025a76b2bff621e1988b9dd0d1204f91663192b
Nestor S. Fuhr <nestor.fuhr@gmail.com>
2020-07-09 18:04:50 -0300

    Tabs

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var eventBus = new Vue();
+
 Vue.component("product", {
   props: {
     premium: {
@@ -32,21 +34,8 @@ Vue.component("product", {
             :disabled="!inStock"
             :class="{ disabledButton: !inStock }">Add to Cart</button>
 
-            <div>
-                <h2>Reviews</h2>
-                <p v-show="!reviews.length">There are no reviews yet.</p>
-                <ul>
-                    <li v-for="review in reviews">
-                       <p>{{ review.name }}</p> 
-                       <p>Rating: {{ review.rating }}</p> 
-                       <p>{{ review.review }}</p> 
-                    </li>
-                </ul>
-            </div>
-            
-            <product-review @review-submitted="addReview"></product-review>
-
         </div>
+            <product-tabs :reviews="reviews"></product-tabs>
 
     </div>
   `,
@@ -80,9 +69,6 @@ Vue.component("product", {
     updateProduct(index) {
       this.selectedVariant = index;
     },
-    addReview(productReview) {
-      this.reviews.push(productReview);
-    },
   },
   computed: {
     title() {
@@ -100,6 +86,11 @@ Vue.component("product", {
       }
       return 2.99;
     },
+  },
+  mounted() {
+    eventBus.$on("review-submitted", (productReview) => {
+      this.reviews.push(productReview);
+    });
   },
 });
 
@@ -158,7 +149,7 @@ Vue.component("product-review", {
           rating: this.rating,
         };
         console.log(productReview);
-        this.$emit("review-submitted", productReview);
+        eventBus.$emit("review-submitted", productReview);
         this.name = null;
         this.review = null;
         this.rating = null;
@@ -168,6 +159,44 @@ Vue.component("product-review", {
         if (!this.rating) this.errors.push("Rating required.");
       }
     },
+  },
+});
+
+Vue.component("product-tabs", {
+  props: {
+    reviews: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+    <div>
+        <span class="tab"
+        :class="{ activeTab:selectedTab === tab }"
+        v-for="(tab, index) in tabs" 
+        :key="index"
+        @click="selectedTab = tab">
+        {{ tab }}</span>
+ 
+        <div v-show="selectedTab === 'Reviews'">
+            <p v-show="!reviews.length">There are no reviews yet.</p>
+            <ul>
+                <li v-for="review in reviews">
+                   <p>{{ review.name }}</p> 
+                   <p>Rating: {{ review.rating }}</p> 
+                   <p>{{ review.review }}</p> 
+                </li>
+            </ul>
+        </div>
+        
+        <product-review v-show="selectedTab === 'Make a Review'"></product-review>  
+    </div>   
+  `,
+  data() {
+    return {
+      tabs: ["Reviews", "Make a Review"],
+      selectedTab: "Reviews",
+    };
   },
 });
 
